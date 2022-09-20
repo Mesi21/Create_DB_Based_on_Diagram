770db2ca845e4b9670bae20d9e68e4edef03c67f
Hamma Azarok <hammaagazarok@gmail.com>
2022-09-20 16:48:43 +0200

    add tables sechma

--- schema.sql
+++ schema.sql
@@ -15,3 +15,30 @@ CREATE TABLE invoices (
     medical_history_id INT REFERENCES medical_histories(id),
     PRIMARY KEY(id)
 );
+
+CREATE TABLE medical_histories (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    admitted_at TIMESTAMP ,
+    patient_id INT REFERENCES patients(id),
+    status VARCHAR(250),
+    PRIMARY KEY(id)
+)
+
+CREATE TABLE treatments (
+    id INT GENERATED ALWAYS AS IDENTITY,
+    type VARCHAR(50),
+    name VARCHAR(100),
+    PRIMARY KEY (id)
+)
+
+CREATE TABLE invoice_items (
+    id GENERATED ALWAYS AS IDENTITY,
+    unit_price decimal,
+    quantity int ,
+    total_price decimal,
+    invoice_id int,
+    treatment_id int,
+    FOREIGN KEY(invoice_id) REFERENCES invoices(id),
+    FOREIGN KEY(treatment_id) REFERENCES treatments(id),
+    PRIMARY KEY (id)
+)
